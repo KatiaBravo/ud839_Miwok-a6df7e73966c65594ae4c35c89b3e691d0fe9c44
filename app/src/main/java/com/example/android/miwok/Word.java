b86746c8c6037d65de80f78d3c8c0ea8768fa0fe
KatiaBravo <katia@alexbravo.com>
2016-11-21 07:21:52 -0800

    let me work at yahoo with having the exact same app

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -16,9 +16,20 @@ public class Word {
     /**Miwok translation for the word*/
     private String mMiwokTranslation;
 
+    /**Image resource ID for the word*/
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
 
     //Get the Default Translation of the word
@@ -30,4 +41,10 @@ public class Word {
     public String getmMiwokTranslation() {
         return mDefaultTranslation;
     }
+
+    //Get the Image Resource id of the image
+    public int getmImageResourceId() { return mImageResourceId;}
+
+    //Returns where or not there is an image for this word
+    public boolean hasImage() { return mImageResourceId != NO_IMAGE_PROVIDED;}
 }
